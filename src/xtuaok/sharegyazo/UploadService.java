38a7df51c9f2cc0d9ba656d3db1d4a60d1239257
Tomonori Tamagawa <xtuaok@gmail.com>
2013-02-21 21:17:09 +0900

    Rewrite Notification for JellyBeans

--- src/xtuaok/sharegyazo/UploadService.java
+++ src/xtuaok/sharegyazo/UploadService.java
@@ -14,22 +14,25 @@ import android.preference.PreferenceManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Bitmap.CompressFormat;
-import android.text.ClipboardManager;
+import android.support.v4.app.NotificationCompat;
+import android.content.ClipboardManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import android.app.IntentService;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 
 public class UploadService extends IntentService {
-    private static final String LOG_TAG = "ShareToGyazo";
+    private static final String LOG_TAG = "GyazoUploadService";
     private static final String DEFAULT_UPLOADER = "http://gyazo.com/upload.cgi";
 
     private static final int NOTIFY_UPLOADING = 0x0;
@@ -43,7 +46,7 @@ public class UploadService extends IntentService {
     private boolean mOpenURL;
     private boolean mShareURL;
     private int mNotifyAction;
-    private Notification mNotification;
+    private Bitmap mBitmap;
     private NotificationManager mNotificationManager;
     private String mErrorMessage = "";
 
@@ -73,17 +76,14 @@ public class UploadService extends IntentService {
         mNotifyAction = Integer.parseInt(prefs.getString(GyazoPreference.PREF_NOTIFY_ACTION, "0"));
 
         mNotificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotification = new Notification(
-                R.drawable.ic_launcher,
-                getString(R.string.dialog_message_uploading),
-                System.currentTimeMillis());
-        mNotification.flags = Notification.FLAG_ONGOING_EVENT;
-        mNotification.setLatestEventInfo(
-                mContext,
-                getString(R.string.app_name),
-                getString(R.string.dialog_message_uploading),
-                null);
-        mNotificationManager.notify(NOTIFY_UPLOADING, mNotification);
+
+        // Notify UPLOADING
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle(getString(R.string.dialog_message_uploading))
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setTicker(getString(R.string.dialog_message_uploading))
+            .setOngoing(true);
+        mNotificationManager.notify(NOTIFY_UPLOADING, builder.build());
 
         mHandler.post(new Runnable() {            
             public void run() {
@@ -97,7 +97,9 @@ public class UploadService extends IntentService {
     
     private void done(String result) {
         Intent intent = new Intent();
-        PendingIntent pIntent = null;
+        PendingIntent intentView = null;
+        PendingIntent intentSend = null;
+        PendingIntent intentCopy = null;
 
         Log.i(LOG_TAG, "Result: " + result);
         if (mErrorMessage != "") {
@@ -116,7 +118,8 @@ public class UploadService extends IntentService {
             return;
         }
         if (mCopyURL) {
-            ((ClipboardManager)getSystemService(CLIPBOARD_SERVICE)).setText(result);
+            ClipData clip = ClipData.newPlainText("url text", result);
+            ((ClipboardManager)getSystemService(CLIPBOARD_SERVICE)).setPrimaryClip(clip);
             mHandler.post(new Runnable() {            
                 public void run() {
                     Toast.makeText(mContext, getString(R.string.toast_copy_url), Toast.LENGTH_LONG).show();
@@ -138,35 +141,59 @@ public class UploadService extends IntentService {
             intent.putExtra(Intent.EXTRA_TEXT, result);
             mContext.startActivity(intent);
         }
-        if (mNotifyAction > 0) {
-            Intent notifyIntent = new Intent();
-            switch (mNotifyAction) {
-            case 1:
-                notifyIntent.setAction(Intent.ACTION_VIEW);
-                notifyIntent.addCategory(Intent.CATEGORY_BROWSABLE);
-                notifyIntent.setData(Uri.parse(result));
-                notifyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                break;
-            case 2:
-                notifyIntent.setAction(Intent.ACTION_SEND);
-                notifyIntent.setType("text/plain");
-                notifyIntent.addCategory(Intent.CATEGORY_DEFAULT);
-                notifyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                notifyIntent.putExtra(Intent.EXTRA_TEXT, result);                
-                break;
+
+        // Notifications
+        Intent notifyIntent;
+
+        // VIEW
+        notifyIntent = new Intent();
+        notifyIntent.setAction(Intent.ACTION_VIEW);
+        notifyIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+        notifyIntent.setData(Uri.parse(result));
+        notifyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intentView = PendingIntent.getActivity(mContext, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // SEND
+        notifyIntent = new Intent();
+        notifyIntent.setAction(Intent.ACTION_SEND);
+        notifyIntent.setType("text/plain");
+        notifyIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        notifyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        notifyIntent.putExtra(Intent.EXTRA_TEXT, result);
+        intentSend = PendingIntent.getActivity(mContext, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // COPY
+        notifyIntent = new Intent(mContext, ClipText.class);
+        notifyIntent.setAction(Intent.ACTION_MAIN);
+        notifyIntent.putExtra(Intent.EXTRA_TEXT, result);
+        notifyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intentCopy = PendingIntent.getActivity(mContext, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // build
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle(getString(R.string.app_name))
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setLargeIcon(mBitmap)
+            .addAction(android.R.drawable.ic_menu_view, "Open", intentView)
+            .addAction(android.R.drawable.ic_menu_send, "Send", intentSend)
+            .addAction(android.R.drawable.ic_menu_set_as, "Copy", intentCopy);
+
+        // Backward Compatibility
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            if (mNotifyAction == 1) {
+                builder.setContentIntent(intentView);
+            } else if (mNotifyAction == 2) {
+                builder.setContentIntent(intentSend);
             }
-            pIntent = PendingIntent.getActivity(mContext, 0, notifyIntent, 0);
+        } else {
+            builder.setContentIntent(intentCopy);
         }
-        Notification notification = new Notification(
-                R.drawable.ic_launcher,
-                getString(R.string.dialog_message_done),
-                System.currentTimeMillis());
-        notification.setLatestEventInfo(
-                mContext,
-                getString(R.string.app_name),
-                getString(R.string.dialog_message_done),
-                pIntent);
-        notification.flags = Notification.FLAG_AUTO_CANCEL;
+
+        Notification notification =
+                new NotificationCompat.BigTextStyle(builder)
+                    .bigText(result)
+                    .setSummaryText(getString(R.string.dialog_message_done))
+                    .build();
         mNotificationManager.cancel(NOTIFY_UPLOADING);
         mNotificationManager.notify(result, NOTIFY_DONE, notification);
     }
@@ -186,6 +213,22 @@ public class UploadService extends IntentService {
             if (options.outMimeType.endsWith("png")) {
                 byte[] img_byte = readBytes(in);
                 in.close();
+                // for notification image
+                int scale = 1;
+                int h = options.outHeight;
+                int w = options.outWidth;
+                while (true) {
+                    if (h / scale > android.R.dimen.notification_large_icon_height ||
+                        w / scale > android.R.dimen.notification_large_icon_width) {
+                        scale = scale * 2;
+                    } else {
+                        break;
+                    }
+                }
+                Log.d(LOG_TAG, "Scale: " + scale);
+                options.inSampleSize = scale;
+                options.inJustDecodeBounds = false;
+                mBitmap = BitmapFactory.decodeByteArray(img_byte, 0, img_byte.length, options);
                 result = postGyazo(cgi, img_byte);
             } else {
                 // TODO: more better solution for OutOfMemory.
@@ -205,6 +248,9 @@ public class UploadService extends IntentService {
                 options.inJustDecodeBounds = false;
                 bitmap = BitmapFactory.decodeStream(in, null, options);
                 in.close();
+                mBitmap = Bitmap.createScaledBitmap(bitmap,
+                        android.R.dimen.notification_large_icon_width,
+                        android.R.dimen.notification_large_icon_height, false);
                 bitmap.compress(CompressFormat.PNG, 100, byteBuffer);
                 bitmap.recycle();
                 result = postGyazo(cgi, byteBuffer.toByteArray());
